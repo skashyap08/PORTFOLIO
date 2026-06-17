09e413396f4ec5bd3b1b1fb11b8d918fb3e507ae
skashyap08 <sakshikashyap9876@gmail.com>
2026-06-17 13:12:55 +0530

    modified:script.js-8

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function reveal(){
     document.querySelectorAll(".reveal");
 
 }
+function reveal(){
+
+    const reveals =
+    document.querySelectorAll(".reveal");
+
+    for(let i = 0; i < reveals.length; i++){
+
+        let windowHeight =
+        window.innerHeight;
+
+        let revealTop =
+        reveals[i].getBoundingClientRect().top;
+
+        let revealPoint = 150;
+
+        if(revealTop < windowHeight - revealPoint){
+            reveals[i].classList.add("active");
+        }
+    }
+}
